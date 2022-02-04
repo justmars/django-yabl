d8535b81ef00c7b5dd0838e4bae13ba75d1872ca
Mars Veloso <mv@Marss-MacBook-Pro.local>
2022-02-05 00:05:22 +0800

    feat: fix #2

--- bookmarks/templatetags/bookmark_util.py
+++ bookmarks/templatetags/bookmark_util.py
@@ -4,6 +4,6 @@ from django.db.models import QuerySet
 register = template.Library()
 
 
-@register.inclusion_tag("bookmarks/items_to_load.html", takes_context=True)
-def populate_bookmark_items(context, qs: QuerySet):
-    return {"items_to_load": qs, "user_profile": context["user_profile"]}
+@register.inclusion_tag("bookmarks/items_to_load.html")
+def populate_bookmark_items(qs: QuerySet, *args, **kwargs):
+    return {"items_to_load": qs, "username": kwargs.get("username", None)}
